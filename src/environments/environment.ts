d60f7de7e044f706f326cc17d9c05dcaa665a464
jose.saldana <josaldmer@gmail.com>
2020-04-25 19:37:00 +0200

    Added style to categories view

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   API_URL: 'http://192.168.1.137:3000/api/v1',
-  //API_URL: 'http://localhost:3000/api/v1',
-  // API_URL: '', // staging
+  // API_URL: 'http://localhost:3000/api/v1',
   production: false
 };
 
